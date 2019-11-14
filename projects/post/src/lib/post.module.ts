dd49ee0cf8cd7fd29a3e65d5b896f694cc3d2723
Ramprit Sahani <rampritsahani@protonmail.com>
2019-11-14 22:23:53 +0530

    adding welcome page

---
+++ projects/post/src/lib/post.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { PostComponent } from './post.component';
+
+
+
+@NgModule({
+  declarations: [PostComponent],
+  imports: [
+  ],
+  exports: [PostComponent]
+})
+export class PostModule { }
---
+++ projects/post/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of post
+ */
+
+export * from './lib/post.service';
+export * from './lib/post.component';
+export * from './lib/post.module';
